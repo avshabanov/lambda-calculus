20e12e9e7b51762ff0d62dabb926415645ee96a0
Alexander Shabanov <avshabanov@gmail.com>
2015-06-27 13:02:23 -0700

    Adding mem usage stats for LambdaCalcIntrusivePtr

--- cpp/LambdaCalcIntrusivePtr.cpp
+++ cpp/LambdaCalcIntrusivePtr.cpp
@@ -5,9 +5,12 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <cassert>
 #include <vector>
+#include <exception>
 
 #include <boost/intrusive_ptr.hpp>
+#include <boost/pool/pool.hpp>
 
 /* perf measurement */
 #include <sys/time.h>
@@ -16,10 +19,63 @@
 using std::endl;
 using std::cout;
 using std::cerr;
+using std::logic_error;
 
 using boost::intrusive_ptr;
 
+
+//
+// Memory helpers
+//
+
+struct AllocFree {
+  int alloc = 0;
+  int free = 0;
+};
+
+struct AtomAllocStats {
+  AllocFree af_Zero_Z;
+  AllocFree af_Zero;
+  AllocFree af_Succ_Z;
+  AllocFree af_Succ_S;
+  AllocFree af_Succ;
+  AllocFree af_Int;
+  AllocFree af_Inc;
+};
+
+static AtomAllocStats gAllocStats;
+
+static void printMemUsageStats() {
+  cout << "MEMORY USAGE:" << endl
+      << "Zero_Z alloc=" << gAllocStats.af_Zero_Z.alloc << ", free=" << gAllocStats.af_Zero_Z.free  << endl
+      << "Zero   alloc=" << gAllocStats.af_Zero.alloc   << ", free=" << gAllocStats.af_Zero.free    << endl
+      << "Succ_Z alloc=" << gAllocStats.af_Succ_Z.alloc << ", free=" << gAllocStats.af_Succ_Z.free  << endl
+      << "Succ_S alloc=" << gAllocStats.af_Succ_S.alloc << ", free=" << gAllocStats.af_Succ_S.free  << endl
+      << "Succ   alloc=" << gAllocStats.af_Succ.alloc   << ", free=" << gAllocStats.af_Succ.free    << endl
+      << "Int    alloc=" << gAllocStats.af_Int.alloc    << ", free=" << gAllocStats.af_Int.free     << endl
+      << "Inc    alloc=" << gAllocStats.af_Inc.alloc    << ", free=" << gAllocStats.af_Inc.free     << endl
+      << endl;
+}
+
+#define OVERRIDE_NEW_AND_DELETE_FOR_CLASS(AtomClass) \
+  static void* operator new (size_t size) { \
+    assert(sizeof(AtomClass) == size); \
+    void* p = malloc(size); \
+    if (p == NULL) { \
+      throw new std::bad_alloc(); \
+    } \
+    ++gAllocStats.af_ ## AtomClass.alloc; \
+    return p; \
+  } \
+  \
+  static void operator delete (void *p) { \
+    free(p); \
+    ++gAllocStats.af_ ## AtomClass.free; \
+  }
+
+//
 // types
+//
 
 class Atom;
 
@@ -45,15 +101,11 @@ public:
 class AbstractAtom : public Atom {
 public:
   virtual int toInt() {
-    cerr << ";; FATAL: toInt should not be called" << endl;
-    abort();
-    return 0;
+    throw new logic_error("toInt should not be called");
   }
 
   virtual AtomPtr f1(const AtomPtr& arg) {
-    cerr << ";; FATAL: f1 should not be called" << endl;
-    abort();
-    return AtomPtr(0);
+    throw new logic_error("f1 should not be called");
   }
 };
 
@@ -64,6 +116,8 @@ public:
   virtual AtomPtr f1(const AtomPtr& arg) {
     return arg;
   }
+
+  OVERRIDE_NEW_AND_DELETE_FOR_CLASS(Zero_Z);
 };
 
 class Zero : public AbstractAtom {
@@ -71,6 +125,8 @@ public:
   virtual AtomPtr f1(const AtomPtr& arg) {
     return AtomPtr(new Zero_Z());
   }
+
+  OVERRIDE_NEW_AND_DELETE_FOR_CLASS(Zero);
 };
 
 // Succ lambda
@@ -85,6 +141,8 @@ public:
   virtual AtomPtr f1(const AtomPtr& z) {
     return s->f1(n->f1(s)->f1(z));
   }
+
+  OVERRIDE_NEW_AND_DELETE_FOR_CLASS(Succ_Z);
 };
 
 class Succ_S : public AbstractAtom { // (% s (% z s ((n s) z)))
@@ -96,6 +154,8 @@ public:
   virtual AtomPtr f1(const AtomPtr& s) {
     return AtomPtr(new Succ_Z(n, s));
   }
+
+  OVERRIDE_NEW_AND_DELETE_FOR_CLASS(Succ_S);
 };
 
 class Succ : public AbstractAtom { // (def succ (% n (% s (% z s ((n s) z)))))
@@ -103,6 +163,8 @@ public:
   virtual AtomPtr f1(const AtomPtr& n) {
     return AtomPtr(new Succ_S(n));
   }
+
+  OVERRIDE_NEW_AND_DELETE_FOR_CLASS(Succ);
 };
 
 // Int
@@ -114,6 +176,8 @@ public:
   Int(int v) : val(v) {}
 
   virtual int toInt() { return val; }
+
+  OVERRIDE_NEW_AND_DELETE_FOR_CLASS(Int);
 };
 
 // Inc
@@ -123,6 +187,8 @@ public:
   virtual AtomPtr f1(const AtomPtr& intAtom) {
     return AtomPtr(new Int(intAtom->toInt() + 1));
   }
+
+  OVERRIDE_NEW_AND_DELETE_FOR_CLASS(Inc);
 };
 
 // helpers
@@ -138,7 +204,7 @@ static void print_formatted_nano_time(long long nano_time) {
 
 // entry point
 
-int main(int argc, const char** argv) {
+static void runDemo(int argc, const char** argv) {
   cout << "Lambda Calc C++ Demo" << endl;
 
   const int count = 10;
@@ -160,14 +226,14 @@ int main(int argc, const char** argv) {
   bool calcN9 = false;
   if (argc > 1 && 0 == strcmp(argv[1], "n9")) {
     calcN9 = true;
-  } 
+  }
 
   if (calcN9) {
     cout << "CalcN9..." << endl;
 
     struct timeval start;
     struct timeval stop;
-  
+
     gettimeofday(&start, NULL);
     int result = n.at(9)->f1(n.at(9))->f1(inc)->f1(zeroVal)->toInt();
     gettimeofday(&stop, NULL);
@@ -190,5 +256,10 @@ int main(int argc, const char** argv) {
   }
 
   cout << "DONE." << endl;
+}
+
+int main(int argc, const char** argv) {
+  runDemo(argc, argv);
+  printMemUsageStats();
   return 0;
 }
